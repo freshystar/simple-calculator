cbae381a3ab82955a459dbf38a753a1bc1c55b39
freshystar <thondipiongange@gmail.com>
2025-01-31 13:14:43 +0100

    feat: Added operations and history file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Creating a simple calculator in rust
 
-This calculator does simple operations like, addition, subtraction, multiplication and division.
+This calculator does simple operations like, addition, subtraction, multiplication, division, factorial, exponential, power and modulus.
 
 ### Pulling the image
  ```sh
@@ -19,3 +19,4 @@ This calculator does simple operations like, addition, subtraction, multiplicati
  $ docker run -it <image/Id> 
  ```
 You will be logged in directly into the container and you will just have to follow the prompts meticulously. The container will be exited immidiately after displaying the result or any in case of any invalid input.
+The file, **calculator.log** stores the records of the calculations you did.
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
 use std::io;
+use factorial::factorial;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 
 fn main() {
- let result: f32;
- println!("Enter the first nuumber:");
- let mut num1 = String::new();
-
- io::stdin().read_line(&mut num1).expect("Failed to read line");
- let num1: f32 = num1.trim().parse().expect("Please type a number");
-
- println!("Enter the second number:");
- let mut num2 = String::new();
- io::stdin().read_line(&mut num2).expect("Failed to read line");
-
- let num2: f32 = num2.trim().parse().expect("Please type a number");
-
- println!("Enter: 1 for +, 2 for -, 3 for *, 4 for /:");
+ let result: f64;
+ println!("Which operation do you want to run?...Enter: 1 for +, 2 for -, 3 for *, 4 for /, 5 for %, 6 for ^, 7 for !, 8 for power:");
 
  let mut sign = String::new();
  io::stdin().read_line(&mut sign).expect("Invalid input");
@@ -26,52 +17,96 @@ fn main() {
         return;
      }
  };
+ if sign == 7 {
+    println!("Enter your number:");
+    let mut num1 = String::new();
 
-match sign {
-    1 => result = num1 + num2,
-    2 => result = num1 - num2,
-    3 => result = num1 * num2,
-    4 => result = num1 /  num2,
+ io::stdin().read_line(&mut num1).expect("Failed to read line");
+ let num1: f64 = num1.trim().parse().expect("Please type a number");
+    match sign {
+        7 => result = factorial(num1),
+        _ => { 
+            println!("Invalid selection");
+            return;
+        }
+    };
+ } else {
+    println!("Enter the first number:");
+ let mut num1 = String::new();
+
+ io::stdin().read_line(&mut num1).expect("Failed to read line");
+ let num1: f64 = num1.trim().parse().expect("Please type a number");
+
+ println!("Enter the second number:");
+ let mut num2 = String::new();
+ io::stdin().read_line(&mut num2).expect("Failed to read line");
+
+ let num2: f64 = num2.trim().parse().expect("Please type a number");
+
+ match sign {
+    1 => result = sum(num1, num2),
+    2 => result = subtraction(num1, num2),
+    3 => result = multiplication(num1, num2),
+    4 => result = division(num1, num2),
+    5 => result = modulus(num1, num2),
+    6 => result = exponential(num1, num2),
+    8 => result = power(num1, num2),
     _ => { 
         println!("Invalid selection");
         return;
     }
 };
 
+log(num1, sign, num2, result);
+}
+
 println!("The result is: {}", result);
+//log(num1, f64, &sign, num2, result);
 
+}
+  mod factorial;
+ 
+fn sum(num1: f64, num2: f64) -> f64 {
+ let add: f64 = num1 + num2;
+ return add;
+}
 
- }
+fn subtraction(num1: f64, num2: f64) -> f64 {
+    let subtract: f64 = num1 - num2;
+    return subtract;
+}
 
-  
+fn multiplication(num1: f64, num2: f64) -> f64 {
+    let multiply: f64 = num1 * num2;
+    return multiply;
+}
 
+fn division(num1: f64, num2: f64) -> f64 {
+    let divide: f64 = num1 / num2;
+    return divide;
+}
 
-// fn sum(num1: f32, num2: f32) -> f32 {
-//  let add: f32 = num1 + num2;
-//  return add;
-// }
+fn power(num1: f64, num2: f64) -> f64 {
+    let pow: f64 = num1.powf(num2);
+    return pow
+}
 
-// fn subtraction(num1: f32, num2: f32) -> f32 {
-//     let subtract: f32 = num1 - num2;
-//     return subtract;
-// }
+fn exponential(num1: f64, num2: f64) -> f64{
+    let expo: f64 = num1 * 10_f64.powf(num2);
+    return expo;
+}
 
-// fn multiplication(num1: f32, num2: f32) -> f32 {
-//     let multiply: f32 = num1 * num2;
-//     return multiply;
-// }
+fn modulus(num1: f64, num2: f64) -> f64 {
+    let modu: f64 = num1 % num2;
+    return modu;
+}
 
-// fn division(num1: f32, num2: f32) -> f32 {
-//     let divide: f32 = num1 / num2;
-//     return divide;
-// }
-
-
-// fn fun_name() {
-//     let  num: f32 = 81.0;
-//     println!("The squreroot of  {} is {}", num, square_root(num))
-// }
-
-// fn square_root(num: f32) -> f32 {
- //       num.sqrt()
- //}
+fn log(num1: f64, sign: i32, num2: f64, result: f64) {
+    let log_entry = format!("{} {} {} = {}\n", num1, sign, num2, result);
+    let mut hist_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("calculator.log")
+        .expect("Cannot open file");
+    hist_file.write_all(log_entry.as_bytes()).expect("Cannot write to file");
+}
